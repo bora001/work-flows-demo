84ee9d42c463c51bd55069e52b0e9a34ef9e9da5
bora <borado.lee7@gmail.com>
2024-11-25 22:26:17 +0900

    [wip] fill the excel

--- src/components/WorkFlow/Workflow.tsx
+++ src/components/WorkFlow/Workflow.tsx
@@ -1,32 +1,31 @@
 "use client";
 
 import { Node, ReactFlow, useReactFlow } from "@xyflow/react";
-
-import { MouseEvent, useState } from "react";
+import Select, { SingleValue } from "react-select";
+import axios from "axios";
+import { ChangeEvent, FormEvent, MouseEvent, useState } from "react";
 import "@xyflow/react/dist/style.css";
+import { PlayArrow } from "@mui/icons-material";
+const initialNodes = [
+  {
+    id: "1",
+    type: "default",
+    data: { label: "START" },
+    position: { x: 100, y: 100 }, // 초기 위치
+  },
+];
 
+const initialEdges = [
+  {
+    id: "edge",
+    source: "1",
+    target: "2",
+    animated: true,
+  },
+];
 const Workflow = () => {
-  const initialNodes = [
-    {
-      id: "1",
-      type: "default",
-      data: { label: "START" },
-      position: { x: 100, y: 100 }, // 초기 위치
-    },
-  ];
-
-  const initialEdges = [
-    {
-      id: "edge",
-      source: "1",
-      target: "2",
-      animated: true,
-    },
-  ];
-
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
-
   const { getViewport } = useReactFlow();
 
   const [currentNode, setCurrentNode] = useState<string | null>(null);
@@ -95,6 +94,53 @@ const Workflow = () => {
     setCurrentNode(null);
   };
 
+  const setData = () => {
+    const newNodeId =
+      nodes.filter((node) => node.data.label.includes("SET DATA")).length + 1;
+    addNode({ label: `SET DATA ${newNodeId}`, type: "set_data" });
+  };
+
+  const [selectInfo, setSelectInfo] = useState({ value: "", option: "" });
+  const addData = (e: FormEvent) => {
+    e.preventDefault();
+    setCurrentNode(null);
+  };
+
+  const options = [
+    { value: "name", label: "name" },
+    { value: "color", label: "color" },
+    { value: "count", label: "count" },
+  ];
+
+  const handleSelectChange = (
+    selectedOption: SingleValue<{ value: string; label: string }>
+  ) => {
+    setSelectInfo((prev) => ({ ...prev, option: selectedOption?.value ?? "" }));
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSelectInfo((prev) => ({ ...prev, value: e.target.value }));
+  };
+
+  const postData = async () => {
+    const { option, value } = selectInfo;
+    try {
+      const response = await axios.post(process.env.NEXT_PUBLIC_ADD_ROW ?? "", {
+        [option]: value,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const onPlay = (e: MouseEvent) => {
+    e.stopPropagation();
+    if (nodes.at(-1)?.data.label.includes("SET DATA")) {
+      postData();
+    }
+  };
+
   return (
     <div style={{ width: "100%", height: "100vh" }} onClick={clickBackground}>
       {currentNode && (
@@ -108,15 +154,34 @@ const Workflow = () => {
           }}
           onClick={(e) => e.stopPropagation()}
         >
+          {currentMenu === "set_data" && (
+            <form onSubmit={addData} className="flex flex-col gap-3 mb-5">
+              <Select
+                options={options}
+                onChange={handleSelectChange}
+                inputValue={selectInfo.option}
+              />
+              <input
+                className="h-9 px-2"
+                onChange={handleInputChange}
+                defaultValue={selectInfo.value}
+              />
+              <button className="btn" type="submit">
+                ADD DATA
+              </button>
+            </form>
+          )}
           {currentMenu === "add_rows" && (
-            <button className="btn">SET DATA</button>
+            <button className="btn" onClick={setData}>
+              SET DATA
+            </button>
           )}
           {nodes.length > 0 && (
-            <button className="btn" onClick={deleteNode}>
+            <button className="btn !bg-[red]" onClick={deleteNode}>
               DELETE
             </button>
           )}
-          {currentMenu !== "add_rows" && (
+          {nodes.length === 1 && (
             <button className="btn" onClick={addRows}>
               ADD ROWS
             </button>
@@ -130,6 +195,13 @@ const Workflow = () => {
         edges={edges}
         style={{ width: "100%", height: "100%" }}
       />
+      <button
+        className="absolute  bottom-10 right-10 flex gap-1 p-2 bg-blue-100 rounded hover:bg-blue-300"
+        onClick={onPlay}
+      >
+        <p>RUN ONCE</p>
+        <PlayArrow />
+      </button>
     </div>
   );
 };
